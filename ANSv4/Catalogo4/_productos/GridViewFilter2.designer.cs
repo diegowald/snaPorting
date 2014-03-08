cfa9734ce84c39d7769c9c12c7811938640b5d98
pbrugnie <pbrugnie@hotmail.com>
2014-03-08 10:47:06 -0300

    revision 4

--- ANSv4/Catalogo4/_productos/GridViewFilter2.designer.cs
+++ ANSv4/Catalogo4/_productos/GridViewFilter2.designer.cs
@@ -28,232 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GridViewFilter2));
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.txtBuscar = new System.Windows.Forms.ToolStripTextBox();
-            this.btnApply0 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.toolStripLabel4 = new System.Windows.Forms.ToolStripLabel();
-            this.cboLinea = new System.Windows.Forms.ToolStripComboBox();
-            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
-            this.cboFamilia = new System.Windows.Forms.ToolStripComboBox();
-            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
-            this.cboMarca = new System.Windows.Forms.ToolStripComboBox();
-            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
-            this.cboModelo = new System.Windows.Forms.ToolStripComboBox();
-            this.toolStripLabel5 = new System.Windows.Forms.ToolStripLabel();
-            this.cboOtros = new System.Windows.Forms.ToolStripComboBox();
-            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
-            this.btnApply = new System.Windows.Forms.ToolStripButton();
-            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.btnClearFilters = new System.Windows.Forms.ToolStripButton();
-            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
-            this.btnRefresh = new System.Windows.Forms.ToolStripButton();
-            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
-            this.lblPorcentajeLinea = new System.Windows.Forms.ToolStripLabel();
-            this.txtPorcentajeLinea = new System.Windows.Forms.ToolStripTextBox();
-            this.tslItems = new System.Windows.Forms.ToolStripLabel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Semáforo = new System.Windows.Forms.DataGridViewButtonColumn();
             this.pnProductoDetalle = new System.Windows.Forms.Panel();
             this.lblProductoDetalle1 = new System.Windows.Forms.Label();
-            this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.pnProductoDetalle.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.txtBuscar,
-            this.btnApply0,
-            this.toolStripSeparator1,
-            this.toolStripLabel4,
-            this.cboLinea,
-            this.toolStripLabel1,
-            this.cboFamilia,
-            this.toolStripLabel2,
-            this.cboMarca,
-            this.toolStripLabel3,
-            this.cboModelo,
-            this.toolStripLabel5,
-            this.cboOtros,
-            this.toolStripSeparator4,
-            this.btnApply,
-            this.toolStripSeparator2,
-            this.btnClearFilters,
-            this.toolStripSeparator3,
-            this.btnRefresh,
-            this.toolStripSeparator5,
-            this.lblPorcentajeLinea,
-            this.txtPorcentajeLinea,
-            this.tslItems});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(682, 27);
-            this.toolStrip1.TabIndex = 0;
-            this.toolStrip1.Text = "toolStrip1";
-            // 
-            // txtBuscar
-            // 
-            this.txtBuscar.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.txtBuscar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtBuscar.Name = "txtBuscar";
-            this.txtBuscar.Size = new System.Drawing.Size(141, 27);
-            // 
-            // btnApply0
-            // 
-            this.btnApply0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btnApply0.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnApply0.Image = ((System.Drawing.Image)(resources.GetObject("btnApply0.Image")));
-            this.btnApply0.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnApply0.Name = "btnApply0";
-            this.btnApply0.Size = new System.Drawing.Size(23, 24);
-            this.btnApply0.Text = "buscar";
-            this.btnApply0.Click += new System.EventHandler(this.btnApply_Click);
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
-            // 
-            // toolStripLabel4
-            // 
-            this.toolStripLabel4.Name = "toolStripLabel4";
-            this.toolStripLabel4.Size = new System.Drawing.Size(35, 24);
-            this.toolStripLabel4.Text = "Línea";
-            // 
-            // cboLinea
-            // 
-            this.cboLinea.Name = "cboLinea";
-            this.cboLinea.Size = new System.Drawing.Size(95, 27);
-            // 
-            // toolStripLabel1
-            // 
-            this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(45, 24);
-            this.toolStripLabel1.Text = "Familia";
-            // 
-            // cboFamilia
-            // 
-            this.cboFamilia.Name = "cboFamilia";
-            this.cboFamilia.Size = new System.Drawing.Size(121, 27);
-            // 
-            // toolStripLabel2
-            // 
-            this.toolStripLabel2.Name = "toolStripLabel2";
-            this.toolStripLabel2.Size = new System.Drawing.Size(40, 24);
-            this.toolStripLabel2.Text = "Marca";
-            // 
-            // cboMarca
-            // 
-            this.cboMarca.Name = "cboMarca";
-            this.cboMarca.Size = new System.Drawing.Size(95, 27);
-            this.cboMarca.SelectedIndexChanged += new System.EventHandler(this.cboMarca_SelectedIndexChanged);
-            // 
-            // toolStripLabel3
-            // 
-            this.toolStripLabel3.Name = "toolStripLabel3";
-            this.toolStripLabel3.Size = new System.Drawing.Size(48, 15);
-            this.toolStripLabel3.Text = "Modelo";
-            // 
-            // cboModelo
-            // 
-            this.cboModelo.Name = "cboModelo";
-            this.cboModelo.Size = new System.Drawing.Size(95, 23);
-            // 
-            // toolStripLabel5
-            // 
-            this.toolStripLabel5.Name = "toolStripLabel5";
-            this.toolStripLabel5.Size = new System.Drawing.Size(36, 15);
-            this.toolStripLabel5.Text = "Otros";
-            // 
-            // cboOtros
-            // 
-            this.cboOtros.Items.AddRange(new object[] {
-            "(todos)",
-            "Ofertas",
-            "Nuevos"});
-            this.cboOtros.Name = "cboOtros";
-            this.cboOtros.Size = new System.Drawing.Size(80, 23);
-            // 
-            // toolStripSeparator4
-            // 
-            this.toolStripSeparator4.ForeColor = System.Drawing.SystemColors.Control;
-            this.toolStripSeparator4.Name = "toolStripSeparator4";
-            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 6);
-            // 
-            // btnApply
-            // 
-            this.btnApply.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btnApply.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnApply.Image = ((System.Drawing.Image)(resources.GetObject("btnApply.Image")));
-            this.btnApply.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnApply.Name = "btnApply";
-            this.btnApply.Size = new System.Drawing.Size(23, 20);
-            this.btnApply.Text = "buscar";
-            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
-            // 
-            // toolStripSeparator2
-            // 
-            this.toolStripSeparator2.ForeColor = System.Drawing.SystemColors.Control;
-            this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 6);
-            // 
-            // btnClearFilters
-            // 
-            this.btnClearFilters.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btnClearFilters.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnClearFilters.Image = ((System.Drawing.Image)(resources.GetObject("btnClearFilters.Image")));
-            this.btnClearFilters.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnClearFilters.Name = "btnClearFilters";
-            this.btnClearFilters.Size = new System.Drawing.Size(23, 20);
-            this.btnClearFilters.Text = "ver todos";
-            this.btnClearFilters.Click += new System.EventHandler(this.btnClearFilters_Click);
-            // 
-            // toolStripSeparator3
-            // 
-            this.toolStripSeparator3.ForeColor = System.Drawing.SystemColors.Control;
-            this.toolStripSeparator3.Name = "toolStripSeparator3";
-            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 6);
-            // 
-            // btnRefresh
-            // 
-            this.btnRefresh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btnRefresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnRefresh.Image = ((System.Drawing.Image)(resources.GetObject("btnRefresh.Image")));
-            this.btnRefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(23, 20);
-            this.btnRefresh.Text = "actualizar";
-            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
-            // 
-            // toolStripSeparator5
-            // 
-            this.toolStripSeparator5.Name = "toolStripSeparator5";
-            this.toolStripSeparator5.Size = new System.Drawing.Size(6, 6);
-            // 
-            // lblPorcentajeLinea
-            // 
-            this.lblPorcentajeLinea.Name = "lblPorcentajeLinea";
-            this.lblPorcentajeLinea.Size = new System.Drawing.Size(23, 15);
-            this.lblPorcentajeLinea.Text = " % ";
-            // 
-            // txtPorcentajeLinea
-            // 
-            this.txtPorcentajeLinea.AutoSize = false;
-            this.txtPorcentajeLinea.MaxLength = 4;
-            this.txtPorcentajeLinea.Name = "txtPorcentajeLinea";
-            this.txtPorcentajeLinea.Size = new System.Drawing.Size(40, 23);
-            this.txtPorcentajeLinea.Text = "0";
-            // 
-            // tslItems
-            // 
-            this.tslItems.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.tslItems.Name = "tslItems";
-            this.tslItems.Size = new System.Drawing.Size(51, 15);
-            this.tslItems.Text = "#Prod. 0";
             // 
             // dataGridView1
             // 
@@ -266,11 +48,11 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Semáforo});
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 27);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 4;
-            this.dataGridView1.Size = new System.Drawing.Size(682, 480);
+            this.dataGridView1.Size = new System.Drawing.Size(682, 507);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
@@ -308,48 +90,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.pnProductoDetalle);
-            this.Controls.Add(this.toolStrip1);
             this.Name = "GridViewFilter2";
             this.Size = new System.Drawing.Size(682, 547);
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.pnProductoDetalle.ResumeLayout(false);
             this.pnProductoDetalle.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripLabel tslItems;
-        private System.Windows.Forms.ToolStripComboBox cboLinea;
-        private System.Windows.Forms.ToolStripComboBox cboFamilia;
-        private System.Windows.Forms.ToolStripComboBox cboMarca;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel4;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
-        private System.Windows.Forms.ToolStripComboBox cboModelo;
-        private System.Windows.Forms.ToolStripTextBox txtBuscar;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel5;
-        private System.Windows.Forms.ToolStripComboBox cboOtros;
-        private System.Windows.Forms.ToolStripButton btnClearFilters;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.ToolStripButton btnRefresh;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
-        private System.Windows.Forms.ToolStripButton btnApply;
         private System.Windows.Forms.Panel pnProductoDetalle;
         private System.Windows.Forms.Label lblProductoDetalle1;
-        private System.Windows.Forms.ToolStripButton btnApply0;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
-        private System.Windows.Forms.ToolStripLabel lblPorcentajeLinea;
-        private System.Windows.Forms.ToolStripTextBox txtPorcentajeLinea;
         private System.Windows.Forms.DataGridViewButtonColumn Semáforo;
     }
 }
