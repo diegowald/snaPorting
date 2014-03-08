1bcdbe73d7911f7a2c6c4b9f05f8e7b7afdd5f36
pbrugnie <pbrugnie@hotmail.com>
2014-03-08 10:21:01 -0300

    revision 3

---
+++ ANSv4/Catalogo4/_productos/DetalleProducto.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Catalogo._productos
+{
+    /// <summary>
+    /// Interaction logic for DetalleProducto.xaml
+    /// </summary>
+    public partial class DetalleProducto : UserControl, util.emitter_receiver.IReceptor<System.Data.DataRow>
+    {
+        public DetalleProducto()
+        {
+            InitializeComponent();
+        }
+
+        public void onRecibir(System.Data.DataRow dato)
+        {
+            txtDetalle.NavigateToString("hola");
+        }
+
+    }
+}
