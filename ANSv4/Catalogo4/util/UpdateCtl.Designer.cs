2ef96ef75094944dc5b097ed19b5770392ecb9da
Diego Ignacio Wald <diego.wald@gmail.com>
2014-03-11 15:20:40 +0100

    Se crea el control para update.

---
+++ ANSv4/Catalogo4/util/UpdateCtl.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Catalogo.util
+{
+    partial class UpdateCtl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fraFRAME1 = new System.Windows.Forms.GroupBox();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.lblNo = new System.Windows.Forms.Label();
+            this.lblSI = new System.Windows.Forms.Label();
+            this.lblStep2 = new System.Windows.Forms.Label();
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.cmdNext = new System.Windows.Forms.Button();
+            this.fraFRAME2 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblProgressUpdate = new System.Windows.Forms.Label();
+            this.lblUpdate = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.fraFRAME1.SuspendLayout();
+            this.fraFRAME2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fraFRAME1
+            // 
+            this.fraFRAME1.Controls.Add(this.lblVersion);
+            this.fraFRAME1.Controls.Add(this.lblProgress);
+            this.fraFRAME1.Controls.Add(this.lblNo);
+            this.fraFRAME1.Controls.Add(this.lblSI);
+            this.fraFRAME1.Controls.Add(this.lblStep2);
+            this.fraFRAME1.Location = new System.Drawing.Point(14, 16);
+            this.fraFRAME1.Name = "fraFRAME1";
+            this.fraFRAME1.Size = new System.Drawing.Size(438, 140);
+            this.fraFRAME1.TabIndex = 0;
+            this.fraFRAME1.TabStop = false;
+            this.fraFRAME1.Text = "Paso 1 de 2";
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(16, 58);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(58, 13);
+            this.lblProgress.TabIndex = 3;
+            this.lblProgress.Text = "lblProgress";
+            // 
+            // lblNo
+            // 
+            this.lblNo.AutoSize = true;
+            this.lblNo.Location = new System.Drawing.Point(19, 114);
+            this.lblNo.Name = "lblNo";
+            this.lblNo.Size = new System.Drawing.Size(306, 13);
+            this.lblNo.TabIndex = 2;
+            this.lblNo.Text = "NO hay actualizaciones dispoibles. click en Cancelar para Salir.";
+            // 
+            // lblSI
+            // 
+            this.lblSI.Location = new System.Drawing.Point(19, 81);
+            this.lblSI.Name = "lblSI";
+            this.lblSI.Size = new System.Drawing.Size(375, 33);
+            this.lblSI.TabIndex = 1;
+            this.lblSI.Text = "Hay na nueva version dispoible, click en Continuar para descargarla o click en Ca" +
+    "ncelar para salir.";
+            // 
+            // lblStep2
+            // 
+            this.lblStep2.Location = new System.Drawing.Point(16, 27);
+            this.lblStep2.Name = "lblStep2";
+            this.lblStep2.Size = new System.Drawing.Size(416, 44);
+            this.lblStep2.TabIndex = 0;
+            this.lblStep2.Text = "Presione click en Continuar para conectar al servidor y verificar actualizaciones" +
+    " disponibles...";
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.Location = new System.Drawing.Point(264, 307);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(75, 23);
+            this.cmdCancel.TabIndex = 1;
+            this.cmdCancel.Text = "Cancelar";
+            this.cmdCancel.UseVisualStyleBackColor = true;
+            this.cmdCancel.Click += new System.EventHandler(this.cmdCancel_Click);
+            // 
+            // cmdNext
+            // 
+            this.cmdNext.Location = new System.Drawing.Point(345, 307);
+            this.cmdNext.Name = "cmdNext";
+            this.cmdNext.Size = new System.Drawing.Size(75, 23);
+            this.cmdNext.TabIndex = 2;
+            this.cmdNext.Text = "Continuar";
+            this.cmdNext.UseVisualStyleBackColor = true;
+            this.cmdNext.Click += new System.EventHandler(this.cmdNext_Click);
+            // 
+            // fraFRAME2
+            // 
+            this.fraFRAME2.Controls.Add(this.progressBar1);
+            this.fraFRAME2.Controls.Add(this.lblProgressUpdate);
+            this.fraFRAME2.Controls.Add(this.lblUpdate);
+            this.fraFRAME2.Location = new System.Drawing.Point(14, 162);
+            this.fraFRAME2.Name = "fraFRAME2";
+            this.fraFRAME2.Size = new System.Drawing.Size(438, 116);
+            this.fraFRAME2.TabIndex = 3;
+            this.fraFRAME2.TabStop = false;
+            this.fraFRAME2.Text = "Paso 2 de 2";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 87);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(274, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // lblProgressUpdate
+            // 
+            this.lblProgressUpdate.AutoSize = true;
+            this.lblProgressUpdate.Location = new System.Drawing.Point(10, 49);
+            this.lblProgressUpdate.Name = "lblProgressUpdate";
+            this.lblProgressUpdate.Size = new System.Drawing.Size(87, 13);
+            this.lblProgressUpdate.TabIndex = 1;
+            this.lblProgressUpdate.Text = "lblprogressupfate";
+            // 
+            // lblUpdate
+            // 
+            this.lblUpdate.AutoSize = true;
+            this.lblUpdate.Location = new System.Drawing.Point(7, 20);
+            this.lblUpdate.Name = "lblUpdate";
+            this.lblUpdate.Size = new System.Drawing.Size(95, 13);
+            this.lblUpdate.TabIndex = 0;
+            this.lblUpdate.Text = "Descargando... $n";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(137, 58);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(52, 13);
+            this.lblVersion.TabIndex = 4;
+            this.lblVersion.Text = "lblVersion";
+            // 
+            // UpdateCtl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.fraFRAME2);
+            this.Controls.Add(this.cmdNext);
+            this.Controls.Add(this.cmdCancel);
+            this.Controls.Add(this.fraFRAME1);
+            this.Name = "UpdateCtl";
+            this.Size = new System.Drawing.Size(472, 351);
+            this.fraFRAME1.ResumeLayout(false);
+            this.fraFRAME1.PerformLayout();
+            this.fraFRAME2.ResumeLayout(false);
+            this.fraFRAME2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox fraFRAME1;
+        private System.Windows.Forms.Button cmdCancel;
+        private System.Windows.Forms.Button cmdNext;
+        private System.Windows.Forms.Label lblNo;
+        private System.Windows.Forms.Label lblSI;
+        private System.Windows.Forms.Label lblStep2;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.GroupBox fraFRAME2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblProgressUpdate;
+        private System.Windows.Forms.Label lblUpdate;
+        private System.Windows.Forms.Label lblVersion;
+    }
+}
---
+++ ANSv4/Catalogo4/util/UpdateCtl.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Catalogo.util
+{
+    public partial class UpdateCtl : UserControl
+    {
+
+        const string vclu = "_vclu.txt";
+        private string tempFolder;
+        private string configFileURL;
+        private string thisVer;
+        private long step;
+        private string name;
+        private string webVer;
+        private string updateURL;
+        private string desktop;
+        private string outFile;
+        private string successFile;
+        private long maxBytes;
+
+        public delegate void CloseRequestHandler(string downloadedFile);
+        public delegate void ConexionErrorHandler(byte estado);
+
+        public CloseRequestHandler CloseRequest;
+        public ConexionErrorHandler ConexionError; 
+
+        public UpdateCtl()
+        {
+            InitializeComponent();
+
+            tempFolder = Environment.GetEnvironmentVariable("TEMP");
+            if (tempFolder.Length == 0)
+            {
+                tempFolder = /*vg.Path +*/ "/Reportes/";
+            }
+            step = 0;
+            //desktop = vg.Path + "/Reportes/";
+            //thisVer = vg.MinBuild;
+            successFile = "";
+        }
+
+        private void cmdCancel_Click(object sender, EventArgs e)
+        {
+            if (CloseRequest != null)
+            {
+                CloseRequest(successFile);
+            }
+        }
+
+        private void cmdNext_Click(object sender, EventArgs e)
+        {
+            long x;
+            cmdCancel.Focus();
+            cmdNext.Enabled = false;
+            step++;
+            fraFRAME1.Visible = step == 1;
+            fraFRAME2.Visible = step != 1;
+            if (step == 1)
+            {
+                if (configFileURL.Length > 0)
+                {
+                    Cursor.Current = Cursors.WaitCursor;
+                    BeginDownload(configFileURL, tempFolder + vclu);
+                    Cursor.Current = Cursors.Default;
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show(
+                    "A configuration download URL was not provided. Live update cannot continue.",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    step--;
+                }
+            }
+            else
+            {
+                lblUpdate.Text = lblUpdate.Text.Replace("$n", "\n" + name)
+                    + "\n\n Esperar un momento, por favor...";
+                BeginDownload(updateURL, desktop + outFile);
+            }
+        }
+
+        private string downloadingFile;
+        private void BeginDownload(string URL, string saveFile)
+        {
+            downloadingFile = saveFile;
+            System.Net.WebClient client = new System.Net.WebClient();
+            client.DownloadProgressChanged += client_DownloadProgressChanged;
+            client.DownloadFileCompleted+=client_DownloadFileCompleted;
+            client.DownloadFileAsync(new Uri(URL), saveFile);
+        }
+
+        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (downloadingFile.EndsWith(vclu))
+            {
+                getConfigValues();
+                lblVersion.Text = String.Format("Version actual: {0}. Version disponible: {1}", thisVer, webVer);
+                if (thisVer.CompareTo(webVer) > 0)
+                {
+                    cmdCancel.Focus();
+                    cmdNext.Enabled = false;
+                }
+                else
+                {
+                    lblSI.Visible = true;
+                    cmdNext.Enabled = true;
+                    cmdNext.Focus();
+                }
+            }
+            else
+            {
+                cmdCancel.Text = "Finalizar";
+                cmdCancel.Focus();
+                if (maxBytes / 1000 < 1000)
+                {
+                    lblProgressUpdate.Text = String.Format("Se descargaron {0} KB, operacion completada.", maxBytes / 1000);
+                }
+                else
+                {
+                    lblProgressUpdate.Text = String.Format("Se descargaron {0} MB, operacion completada.", maxBytes / 1000000);
+                }
+                successFile = downloadingFile;
+            }
+        }
+
+        private void getConfigValues()
+        {
+            try
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(tempFolder + vclu);
+                name = "";
+                webVer = "";
+                updateURL = "";
+                for (int lineNo = 1; lineNo <= 3; lineNo++)
+                {
+                    string line = file.ReadLine();
+                    switch (lineNo)
+                    {
+                        case 1:
+                            name = line;
+                            break;
+                        case 2:
+                            webVer = line;
+                            break;
+                        case 3:
+                        default:
+                            updateURL = line;
+                            int spos = updateURL.LastIndexOf('/');
+                            outFile = updateURL.Substring(spos);
+                            break;
+                    }
+                }
+                System.IO.File.Delete(tempFolder + vclu);
+            }
+            catch
+            {
+            }
+        }
+
+        void client_DownloadProgressChanged(object sender, System.Net.DownloadProgressChangedEventArgs e)
+        {
+            if (!downloadingFile.EndsWith(vclu))
+            {
+                if (e.TotalBytesToReceive / 1000 < 1000)
+                {
+                    lblProgressUpdate.Text = String.Format("{0} KB de {1} KB descargados.", e.BytesReceived / 1000, e.TotalBytesToReceive / 1000);
+                }
+                else
+                {
+                    lblProgressUpdate.Text = String.Format("{0} MB de {1} MB descargados", e.BytesReceived / 1000000, e.TotalBytesToReceive / 1000000);
+                }
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = (int)e.TotalBytesToReceive;
+                progressBar1.Value = (int)e.BytesReceived;
+                maxBytes = e.TotalBytesToReceive;
+            }
+        }
+
+    }
+}
