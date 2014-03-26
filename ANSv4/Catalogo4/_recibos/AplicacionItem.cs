b992b4f3a1ab55b91cd905e7415c45ab3173ada4
Diego Ignacio Wald <diego.wald@gmail.com>
2014-03-26 20:53:33 +0100

    Se mapean los web Service clients a Catalogo4 Se crean las clases correspondientes a recibo.

---
+++ ANSv4/Catalogo4/_recibos/AplicacionItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Catalogo._recibos
+{
+    public class AplicacionItem
+    {
+
+        //variables locales para almacenar los valores de las propiedades
+        //copia local
+        private string mvarConcepto;
+        //copia local
+        private float mvarImporte;
+        //copia local
+        private string mvarObservaciones;
+
+        public string Observaciones
+        {
+            get { return mvarObservaciones; }
+            set { mvarObservaciones = value; }
+        }
+
+        public float Importe
+        {
+            get { return mvarImporte; }
+            set { mvarImporte = value; }
+        }
+
+        public string Concepto
+        {
+            get { return mvarConcepto; }
+            set { mvarConcepto = value; }
+        }
+
+    }
+}
---
+++ ANSv4/Catalogo4/_recibos/DeducirItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Catalogo._recibos
+{
+    public class DeducirItem
+    {
+        //variables locales para almacenar los valores de las propiedades
+        private string mvarConcepto;
+        private float mvarImporte;
+        private bool mvarPorcentaje;
+
+        private bool mvarDeduAlResto;
+        public bool DeduAlResto
+        {
+            get { return mvarDeduAlResto; }
+            set { mvarDeduAlResto = value; }
+        }
+
+        public bool Porcentaje
+        {
+
+            get { return mvarPorcentaje; }
+            set { mvarPorcentaje = value; }
+        }
+
+        public float Importe
+        {
+            get { return mvarImporte; }
+            set { mvarImporte = value; }
+        }
+
+        public string Concepto
+        {
+            get { return mvarConcepto; }
+            set { mvarConcepto = value; }
+        }
+    }
+}
