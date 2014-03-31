58565a43f773cbd8745dfe183045a3ba2fc8f1a3
Diego Ignacio Wald <diego.wald@gmail.com>
2014-03-31 20:16:04 +0200

    Se corrigen errores al incorporar transacciones.

--- ANSv4/Catalogo4/_clientes/UpdateClientes.cs
+++ ANSv4/Catalogo4/_clientes/UpdateClientes.cs
@@ -223,6 +223,10 @@ namespace Catalogo._clientes
                 cmd.Connection = Conexion;
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
                 cmd.CommandText = "usp_Clientes_add";
+                if (Global01.TranActiva != null)
+                {
+                    cmd.Transaction = Global01.TranActiva;
+                }
                 cmd.ExecuteNonQuery();
 
                 cmd = null;
@@ -274,6 +278,10 @@ namespace Catalogo._clientes
             cmd.Connection = Conexion;
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
             cmd.CommandText = "usp_CtaCte_add";
+            if (Global01.TranActiva != null)
+            {
+                cmd.Transaction = Global01.TranActiva;
+            }
             cmd.ExecuteNonQuery();
 
             cmd = null;
