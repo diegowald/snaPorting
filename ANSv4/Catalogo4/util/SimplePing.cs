02a92cc55ac3f888e71fbf4c1fb6b8ba7deb172e
Diego Ignacio Wald <diego.wald@gmail.com>
2014-04-18 14:30:33 +0200

    Se reemplaza la funcionalidad ping, por un "HTTP Ping" que establece conexion http con una url

--- ANSv4/Catalogo4/util/SimplePing.cs
+++ ANSv4/Catalogo4/util/SimplePing.cs
@@ -7,7 +7,7 @@ namespace Catalogo.util
 {
     public static class SimplePing
     {
-        public static bool ping(string ipAddress, int timeOut)
+        /*public static bool ping(string ipAddress, int timeOut)
         {
             ipAddress = "8.8.8.8";
 
@@ -16,6 +16,27 @@ namespace Catalogo.util
             System.Net.NetworkInformation.PingReply reply = p.Send(ipAddress, timeOut);
 
             return reply.Status == System.Net.NetworkInformation.IPStatus.Success;
+        }*/
+
+        public static bool ping(string ipAddress, int timeOut)
+        {
+            string url = ipAddress.StartsWith("http://") ? ipAddress : "http://" + ipAddress;
+            System.Net.HttpWebRequest request = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(url);
+            request.AllowAutoRedirect = false; // find out if this site is up and don't follow a redirector
+            request.Method = "HEAD";
+            request.Timeout = timeOut;
+            try
+            {
+                var response = request.GetResponse();
+                // do something with response.Headers to find out information about the request
+                return true;
+            }
+            catch (System.Net.WebException wex)
+            {
+                util.errorHandling.ErrorLogger.LogMessage(wex);
+                //set flag if there was a timeout or some other issues
+                return false;
+            }
         }
     }
 }
