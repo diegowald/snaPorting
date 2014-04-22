da5896d147f6f14dd293c209c7ceac7b50c315b2
Diego Ignacio Wald <diego.wald@gmail.com>
2014-04-22 19:40:38 +0200

    Se crea el form para passwords.

---
+++ ANSv4/Catalogo4/_preferencias/passwordFrm.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Catalogo._preferencias
+{
+    /// <summary>
+    /// Interaction logic for passwordFrm.xaml
+    /// </summary>
+    public partial class passwordFrm : Window
+    {
+        private string _password;
+        public passwordFrm(string password)
+        {
+            InitializeComponent();
+            _password = password;
+        }
+
+        private void OKButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (txtPwd.Password == _password)
+            {
+                DialogResult = true;
+                Close();
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Clave incorrecta", "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Information);
+            }
+        }
+
+        private void CancelButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
